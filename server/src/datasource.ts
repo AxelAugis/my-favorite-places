968f887ec2a5c473609ef792126a8568e268ee80
Alexis <velris.dev@gmail.com>
2026-03-17 09:55:31 +0100

    Test patch

--- server/src/datasource.ts
+++ server/src/datasource.ts
@@ -1,10 +1,12 @@
 import { DataSource } from "typeorm";
 
+const isJestRuntime = process.env.JEST_WORKER_ID !== undefined;
+
 const datasource = new DataSource({
   type: "better-sqlite3",
-  database: "./db.sqlite",
+  database: isJestRuntime ? ":memory:" : "./db.sqlite",
   entities: ["./src/entities/**/*.{js,ts}"],
-  logging: true,
+  logging: !isJestRuntime,
   synchronize: true,
 });
 
